6d56e621e0748bae03a8e7baf2391d3175451d3b
Malik Dunston <hello@malikdunston.com>
2021-08-13 23:23:23 -0400

    nav-top/btm, reactComp as hamburger, search, css svg anim

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import {Link} from "react-router-dom";
+import {ReactComponent as SearchIcon} from "../assets/icons/search.svg";
+import {ReactComponent as MenuOpen} from "../assets/icons/menu_open.svg";
 
 class Navigation extends Component { 
 constructor(props){
@@ -18,24 +20,32 @@ navClick(){
 	this.setState(prevState=>{return {open: !prevState.open}})
 }
 render(){ return (
-	<nav className="">
-		<Link to="/" className="navbar-brand">
-			<img src={this.props.logo} width="30" height="30" className="d-inline-block align-top" alt=""/>
-			<div>NPS</div>
-		</Link>
-		<button type="button" className="search" onClick={this.navClick}>🔎︎</button>
-		<button type="button" onClick={this.navClick}>☰</button>
-		{this.state.menu.length ? <div className={"menu " + (this.state.open ? "show" : "")} >
-			<ul>
-				{this.state.menu.map(m=>{
-					return <li className="">
-						<Link to={m[1]} className="">
-							{m[0]}
-						</Link>
-					</li>
-				})}
-			</ul>
-		</div> : ""}
+	<nav>
+		<div className="nav-top">
+			<Link to="/" className="logo">
+				<img src={this.props.logo} width="30" height="30" className="d-inline-block align-top" alt=""/>
+				<div>NPS</div>
+			</Link>
+			<button type="button" className="search" onClick={this.navClick}>
+				<SearchIcon />
+			</button>
+			<button type="button" onClick={this.navClick}>
+				<MenuOpen />
+			</button>
+		</div>
+		<div className="nav-bottom">
+			{this.state.menu.length ? <div className={"menu " + (this.state.open ? "show" : "")} >
+				<ul>
+					{this.state.menu.map(m=>{
+						return <li className="">
+							<Link to={m[1]} className="">
+								{m[0]}
+							</Link>
+						</li>
+					})}
+				</ul>
+			</div> : ""}
+		</div>
 	</nav>
 ) }
-} export default  Navigation;
+} export default Navigation;
