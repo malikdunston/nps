6957118c0cefac48a500fb24f6523f4d9dc0a989
Malik Dunston <hello@malikdunston.com>
2021-08-14 00:34:02 -0400

    nav search

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,6 +8,11 @@ constructor(props){
 	super(props);
 	this.state = {
 		open: false,
+		search: {
+			open: false,
+			valid: false,
+			q: ""
+		},
 		menu: [
 			// ["home", "/"],
 			// ["article", "/article"],
@@ -15,9 +20,31 @@ constructor(props){
 		]
 	}
 	this.navClick = this.navClick.bind(this);
+	this.toggleSearch = this.toggleSearch.bind(this);
 }
 navClick(){
 	this.setState(prevState=>{return {open: !prevState.open}})
+}
+validateSearch(e){
+	const len = 3;
+	this.setState(prevState=>{return {search: {
+		...prevState.search,
+		q: e.target.value,
+		valid: e.target.value.length >= len ? true : false
+	}}})
+}
+toggleSearch(e){
+	const input = e.currentTarget.parentNode.querySelector("input");
+	input.focus();
+	input.select();
+	if(this.state.search.valid){
+		window.location.href = `${process.env.PUBLIC_URL}/search/${this.state.search.q}`;
+	}
+	this.setState(prevState=>{return {search: {
+		open: !prevState.search.open,
+		valid: false,
+		q: ""
+	}}})
 }
 render(){ return (
 	<nav>
@@ -26,12 +53,15 @@ render(){ return (
 				<img src={this.props.logo} width="30" height="30" className="d-inline-block align-top" alt=""/>
 				<div>NPS</div>
 			</Link>
-			<button type="button" className="search" onClick={this.navClick}>
-				<SearchIcon />
-			</button>
-			<button type="button" onClick={this.navClick}>
+			<div className={"search " + (this.state.search.open ? "open" : "")}>
+				<input type="text" onChange={(e)=>{this.validateSearch(e)}} />
+				<div className="button" onClick={(e)=>{this.toggleSearch(e)}}>
+					<SearchIcon />
+				</div>
+			</div>
+			<div className="button" onClick={this.navClick}>
 				<MenuOpen />
-			</button>
+			</div>
 		</div>
 		<div className="nav-bottom">
 			{this.state.menu.length ? <div className={"menu " + (this.state.open ? "show" : "")} >
