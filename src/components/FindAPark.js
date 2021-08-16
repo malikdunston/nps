f194ef92baa47224f3fc845ebd14403a76936af4
Malik Dunston <hello@malikdunston.com>
2021-08-16 12:19:14 -0400

    findapark > datalist, onKeyDown for 'Enter'

--- src/components/FindAPark.js
+++ src/components/FindAPark.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class FindAPark extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
 			states: {
@@ -9,8 +9,8 @@ class FindAPark extends Component {
 			}
 		}
 	}
-	submit(e){
-		if(this.state.states.selected){
+	submit(e) {
+		if (this.state.states.selected) {
 			e.preventDefault();
 			window.location.href = `${process.env.PUBLIC_URL}/search/parks/${this.state.states.selected}`;
 		}
@@ -18,13 +18,18 @@ class FindAPark extends Component {
 	render() {
 		return (
 			<div className="find-a-park">
-				<select onChange={(e)=>{
-					this.setState({states: {selected: e.target.value}})}}>
-					{this.props.states.map(s=>{
+				<input list="find-a-park" 
+					placeholder="Find Your Park by State" 
+					onKeyDown={(e) => e.key === "Enter" && this.submit(e)}
+					onChange={(e) => {
+						this.setState({ states: { selected: e.target.value } })
+				}} />
+				<datalist id="find-a-park">
+					{this.props.states.map(s => {
 						return <option value={s[0]}>{s[1]}</option>
 					})}
-				</select>
-				<button type="submit" onClick={(e)=>{this.submit(e);}}>Find A Park</button>
+				</datalist>
+				<button type="submit" onClick={(e) => { this.submit(e); }}>Find A Park</button>
 			</div>
 		);
 	}
