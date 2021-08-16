b202064f28e57ae91b859b3209e7395d56aa1981
Malik Dunston <hello@malikdunston.com>
2021-08-16 12:05:20 -0400

    FindApark js/css

--- src/components/FindAPark.js
+++ src/components/FindAPark.js
@@ -9,32 +9,23 @@ class FindAPark extends Component {
 			}
 		}
 	}
+	submit(e){
+		if(this.state.states.selected){
+			e.preventDefault();
+			window.location.href = `${process.env.PUBLIC_URL}/search/parks/${this.state.states.selected}`;
+		}
+	}
 	render() {
 		return (
-			<form className="py-4" novalidate>
-				<div className="container form-group px-3">
-					<h2 className="text-center">Find A National Park</h2>
-					<label for="select-a-state">Select A State</label>
-					<select id="select-a-state"
-						className="form-control"
-						onChange={(e)=>{
-							this.setState({states: {selected: e.target.value}})
-						}}>
-						<option value="">Select A State...</option>
-						{this.props.states.map(s=>{
-							return <option value={s[0]}>{s[1]}</option>
-						})}
-					</select>
-				</div>
-				<div className="container text-center">
-					<button type="submit" className="btn btn-sm btn-primary" onClick={(e)=>{
-						if(this.state.states.selected){
-							e.preventDefault();
-							window.location.href = `${process.env.PUBLIC_URL}/search/parks/${this.state.states.selected}`
-						}
-					}}>Search</button>
-				</div>
-			</form>
+			<div className="find-a-park">
+				<select onChange={(e)=>{
+					this.setState({states: {selected: e.target.value}})}}>
+					{this.props.states.map(s=>{
+						return <option value={s[0]}>{s[1]}</option>
+					})}
+				</select>
+				<button type="submit" onClick={(e)=>{this.submit(e);}}>Find A Park</button>
+			</div>
 		);
 	}
 }
