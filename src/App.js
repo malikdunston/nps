d83d0b273d92d2fe489226549ee75560be6fd2af
Malik Dunston <hello@malikdunston.com>
2021-11-02 13:36:08 -0400

    from simple_slider.git

--- src/App.js
+++ src/App.js
@@ -9,10 +9,98 @@ import { Component } from "react";
 	import Article from "./pages/article/Article.js";
 	import Park from "./pages/park/Park.js";
 	import Search from "./pages/search/Search.js";
+	import Slider from './components/Slider/Slider';
 
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			pets: {
+				dogs: [
+					{
+						img: "./assets/img/doberman.jpg",
+						content: {
+							title: "Doberman Pinscer",
+							content: "A lovely breed, indeed"
+						}
+					},
+					{
+						img: "./assets/img/husky.jpg",
+						content: {
+							title: "Siberian Husky",
+							content: "A lovely breed, indeed"
+						}
+					},
+					{
+						img: "./assets/img/pitbull.jpg",
+						content: {
+							title: "American Pit Bull Terrier",
+							content: "A lovely breed, indeed"
+						}
+					},
+				],
+				cats: [
+					{
+						img: "./assets/img/siamese.jpg",
+						content: {
+							title: "One",
+							content: "A lovely breed, indeed"
+						}
+					},
+					{
+						img: "./assets/img/bengal.jpg",
+						content: {
+							title: "Two",
+							content: "A lovely breed, indeed"
+						}
+					},
+					{
+						img: "./assets/img/persian.jpg",
+						content: {
+							title: "Three",
+							content: "A lovely breed, indeed"
+						}
+					}
+				],
+				sample: [
+					{
+						img: "./assets/img/siamese.jpg",
+						content: {
+							title: "1/3",
+						}
+					},
+					{
+						img: "./assets/img/bengal.jpg",
+						content: {
+							title: "2/3",
+						}
+					},
+					{
+						img: "./assets/img/persian.jpg",
+						content: {
+							title: "3/3",
+						}
+					}
+				]
+			}
+		}
+	}
 	render() {return <div className={"App " + (this.props.location.pathname.split("/")[1] || "home")}>
 		<Navigation logo={Logo}/>
+
+	{/* testing... */}
+		<Slider slides={this.state.pets.dogs} axis={"Y"} height={300} controls={["index", "arrows"]} transition={100} startAt={3}/>
+	{/* default... */}
+		<Slider slides={this.state.pets.dogs}/>
+	{/* all settings custom...  */}
+		<Slider slides={[ ...this.state.pets.cats, ...this.state.pets.dogs ]}
+			axis="Y"
+			height={250}
+			width={250}
+			transition={200}
+			controls={["index"]}
+			startAt={2}/>
+
 		<Route exact path="/"
 			render={()=>{
 				return <Home/>
---
+++ src/components/Slider/Slide.js
@@ -0,0 +1,21 @@
+import React from 'react'
+export default function Slide({slide}) {
+	return <div className="slide" style={{
+		position: "relative",
+		[slide.axis === "Y" ? "minHeight" : "minWidth"]: "100%"
+	}}>
+		<img src={process.env.PUBLIC_URL + ( slide.img.split(".")[1] + "." + slide.img.split(".")[2])} 
+			style={{ objectFit:"cover", width:"100%", height:"100%", position:"absolute" }}
+			alt={slide.content.title}/>
+		<div className="slider-content"
+			style={{
+				bottom:"0",
+				width:"100%",
+				position:"absolute",
+				color: slide.index >= 4 || slide.index <= 0 ? "red" : "blue"
+			}}>
+			<h2>{slide.content.title}</h2>
+			<p>{slide.content.content}</p>
+		</div>
+	</div>
+}
