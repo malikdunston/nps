b81592f9153e9972db5b4526d231cf8460bea696
Malik Dunston <hello@malikdunston.com>
2021-11-05 17:03:08 -0400

    home -> news & parks in dom, with slider.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,42 +4,39 @@ import getData from "./../Components/getData";
 import Slider from "./../Components/SliderJS/Slider";
 function Home( props ) {
 	const [ news, setNews ] = useState([]);
-	// const [ parks, setParks ] = useState([]);
+	const [ parks, setParks ] = useState([]);
 	const getNews = async () => {
 		let news = await props.getData("newsreleases");
 		news = news.data.filter(f=>f.image.url);
 		news.length = 7;
 		setNews( news );
 	}
-	// const getParks = () => {
-	// 	setParks( async (oldNews) => {
-	// 		let parks = await props.getData("parks");
-	// 		parks.data.length = 18;
-	// 		return parks.data
-	// 	} );
-	// }
+	const getParks = async () => {
+		let parks = await props.getData("parks");
+		parks.data.length = 18;
+		setParks( parks.data );
+	}
 	useEffect(() => {
 		getNews();
+		getParks();
 	}, [])
 	return <div>
-
+		<h1>National Parks Service</h1>
 		{news ? <Slider slides={news}
 			height={250}
 			transition={200}
 			controls={false} /> : ""}
-			
-		{/* <section id="popular" className="container py-4 px-3">
+{/* 
+	When we return here, consider replacing this with a <Slider></Slider> 
+	with scroll="on"...
+*/}
+		{parks ? <section>
 			<h1>View Parks</h1>
-			{parks ? parks.map(p=>{
-				return <Link to={"/park/" + p.parkCode} className="card">
-					<img className="" src={p.images[0].url} alt={p.images[0].altText}/>
-					<div className="card-body">
-						<h5 className="card-title">{p.name}</h5>
-					</div>
-				</Link>
-			}) : ""}
-		</section> */}
-
+			{parks.map( p => <Link to={"/park/" + p.parkCode} className="card">
+				<img src={p.images[0].url} alt={p.images[0].altText}/>
+				<h5 className="card-title">{p.name}</h5>
+			</Link>)}
+		</section> : ""}
 	</div>
 } 
 export default getData( Home );
