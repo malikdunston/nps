7551f7cb480b1c68a7b9e11bde80abe072b403c4
Malik Dunston <hello@malikdunston.com>
2021-11-05 20:36:30 -0400

    navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Route, Link, withRouter } from "react-router-dom";
-
-export default class Navigation extends Component {
-	render() {
-		return (
-			<div>
-				Nav
-				<Link to={"/"}>Home</Link>
-				<Link to={"/search"}>Search</Link>
-			</div>
-		)
-	}
+export default function Navigation ( props ) {
+	return <div>
+		Nav
+		<Link to={"/"}>Home</Link>
+		<Link to={"/parks/all"}>All Parks</Link>
+		<Link to={"/news"}>News</Link>
+		<Link to={"/search"}>Search</Link>
+	</div>
 }
