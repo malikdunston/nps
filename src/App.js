dbb8111cded22ce1a1028e6f70f6741fbacb5db3
Malik Dunston <hello@malikdunston.com>
2021-11-05 23:46:42 -0400

    100% rfc now.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import { Component } from "react";
+import React, { useState } from "react";
 import { Route, Link, withRouter } from "react-router-dom";
 import Logo from "./assets/images/logo.svg";
 import Navigation from "./Components/Navigation";
 import Home from "./Pages/Home";
-import HomeVideo from "./assets/videos/hero_video.mp4";
-import HomeVideoWEBM from "./assets/videos/hero_video.webm";
-class App extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			heroData: [
-				[HomeVideo, "mp4"],
-				[HomeVideoWEBM, "webm"]
-			]
-		}
-	}
-	render() {return <div className={"App " + (this.props.location.pathname.split("/")[1] || "home")}>
+import video_mp4 from "./assets/videos/hero_video.mp4";
+import video_webm from "./assets/videos/hero_video.webm";
+function App( props ) {
+	const [ hero, setHero ] = useState([
+		[video_mp4, "mp4"],
+		[video_webm, "webm"]
+	])
+	return <div className={"App " + (props.location.pathname.split("/")[1] || "home")}>
 		<Navigation logo={Logo}/>
 		<Route path="/search"
 			render={()=>{
@@ -23,8 +18,8 @@ class App extends Component {
 			}}/>
 		<Route path="/"
 			render={()=>{
-				return <Home heroData={this.state.heroData}/>
+				return <Home heroData={hero}/>
 			}}/>
-	</div>};
-}; 
+	</div>
+}
 export default withRouter(App);
